ec6b7e848dd92f21df5a07878de74f41189f19a6
Andri Martin <113878679+RexxS8@users.noreply.github.com>
2023-12-03 12:39:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ pip install matplotlib
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
